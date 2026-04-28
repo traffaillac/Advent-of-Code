fb0cb4b3aba336e65e68e801ce3ffa63809978a7
Thibault Raffaillac <traf@ik.me>
2026-04-28 08:19:56 +0200

    2015 day 5 fun with conciseness

---
+++ 2015/day5.py
@@ -0,0 +1,11 @@
+I = open(0).read().split()
+
+# day 1
+def isnice1(w):
+	return sum(c in "aeiou" for c in w)>=3 and any(a==b for a, b in zip(w, w[1:])) and all(s not in w for s in ["ab", "cd", "pq", "xy"])
+print(sum(isnice1(w) for w in I))
+
+# day 2
+def isnice2(w):
+	return any(w[i:i+2] in w[i+2:] for i in range(len(w)-1)) and any(a==b for a, b in zip(w, w[2:]))
+print(sum(isnice2(w) for w in I))
